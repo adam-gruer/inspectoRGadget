039d065aa94a9a26a96877c9f1a7097529ae962d
adam-gruer <adamgruer@icloud.com>
2018-05-28 12:49:11 +1000

    replaces purrr::walk with importIntoEnv for addding gogo_functions to gadget_arms environment. Detaches gadget_arms from search path is it already is attached so that more gogo_ functions can be added. Creates new environment otherwise

--- R/gogo_gadget_arms.R
+++ R/gogo_gadget_arms.R
@@ -25,9 +25,11 @@ gogo_gadget_arms <-  function(package) {
 
   # vector of all names (i.e. functions and objects) exported ny the namespace
 
+  exp_names <- getNamespaceExports(ns)
 
-  # funcs <- ls(paste0("package:",package))
-  names <- getNamespaceExports(ns)
+  # append "gogo_" to the names
+  gogo_names <- paste0("gogo_",exp_names)
+
 
   # Create new environment gadget_arms
   # assign each function to gadget environment
@@ -35,12 +37,24 @@ gogo_gadget_arms <-  function(package) {
   # credit: Hadley Wickhaam https://github.com/adam-gruer/inspectoRGadget/issues/2
   # todo if gadget_arms is already in search path copy it %>% detach it %>% add to it %>%  reattach it
 
-  gadget_arms  <- new.env()
+    new_env <- "gadget_arms"
 
-  purrr::walk(names ,  function(.n){
-    assign(paste0("gogo_",.n),
-           getExportedValue(ns, .n),
-           envir = gadget_arms)
-  })
+    if (any(search() == new_env)) {
+            gadget_arms <- detach(new_env)
+
+          } else {
+            gadget_arms  <- new.env()
+          }
+
+    importIntoEnv(impenv = gadget_arms,
+                  impnames = gogo_names ,
+                  expenv = ns,
+                  expnames = exp_names)
+
+  # purrr::walk(names ,  function(.n){
+  #   assign(paste0("gogo_",.n),
+  #          getExportedValue(ns, .n),
+  #          envir = gadget_arms)
+  # })
   attach(gadget_arms)
 }
